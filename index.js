e79ae25d6cf5a7aca3c5fb48721bb89970854a07
Regina <reina5425@gmail.com>
2023-06-26 12:23:31 +0300

    app handlers and error handlers

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ import { userRouter } from "./users/users.js";
 const port = 8000;
 const app = express();
 
+// обработчик событий на приложение
+app.use((req, res, next) => {
+	console.log('Время ', Date.now());
+	next();
+})
+
 app.get("/hello", (req, res) => {
   // res.send("Hello world!");
-	res.end();
+	// res.end();
+	throw new Error('Error!!!');
 });
 
 app.use("/users", userRouter);
+
+// обработка ошибок
+app.use((err, req, res, next) => {
+	console.log(err.message);
+	res.status(500).send(err.message);
+})
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
