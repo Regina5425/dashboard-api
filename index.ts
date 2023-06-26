58e7fef865080184b2372231f58a62292c5361ae
Regina <reina5425@gmail.com>
2023-06-26 13:54:11 +0300

    add typescript in the project

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import { userRouter } from "./users/users.js";
 
 const port = 8000;
@@ -19,7 +19,7 @@ app.get("/hello", (req, res) => {
 app.use("/users", userRouter);
 
 // обработка ошибок
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	console.log(err.message);
 	res.status(500).send(err.message);
 })
