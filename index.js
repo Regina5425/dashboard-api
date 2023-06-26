d8fdae351b971cae0bfbf9cd59c5f0024dad22a7
Regina <reina5425@gmail.com>
2023-06-26 11:56:49 +0300

    routing and client response

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ import express from "express";
 const port = 8000;
 const app = express();
 
+// app.all("/hello", (req, res, next) => {
+//   console.log("All");
+//   next();
+// });
+
 app.get("/hello", (req, res) => {
+  // res.set("Content-Type", "text/plain");
+  // res.append("Warning", "code");
+  // res.cookie("token", "fsfsfsdf", {
+  //   domain: "",
+  //   path: "/",
+  //   secure: true,
+  //   expires: 60000,
+  // });
+	// res.clearCookie('token', {path});
   res.send("Hello world!");
 });
 
